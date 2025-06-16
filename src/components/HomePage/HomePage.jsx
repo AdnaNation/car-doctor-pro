83fe92f6277a8bebc7e3a73c8cda5268ffaac243
Adnan <azizulislamadnan@gmail.com>
2025-06-16 12:44:44 +0600

    add: services

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "../shared/NavBar";
 import Banner from "./Banner";
 import About from "./About";
+import Services from "./Services";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
       <About />
+      <Services />
     </div>
   );
 };
